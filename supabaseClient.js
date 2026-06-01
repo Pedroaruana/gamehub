ba3b0774d1e286de854816f6d66cede400953ce1
PEDRO <aruanapedro@gmail.com>
2026-06-01 14:57:47 -0300

    update supabase key

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://azufrvextbcxwwvqwmiy.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6dWZydmV4dGJjeHd3dnF3bWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MzA0NzQsImV4cCI6MjA5NTIwNjQ3NH0.Z2S163L62pZA_NdPMqZibD7CrRfUXZTfGsnvktn9G2A";
+const supabaseKey = "sb_publishable_K0zqD8_Hkw7XC8aXraktKg_z39LxOdG";
 
 window.supabaseClient = window.supabase.createClient(
   supabaseUrl,
